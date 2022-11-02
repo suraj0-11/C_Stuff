bf2c76ef48e2783353c1df6570225f70ef731748
Suraj S <116282102+suraj0-11@users.noreply.github.com>
2022-11-02 22:53:32 +0530

    Add files via upload

---
+++ test.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+void create(int n)
+{
+    int i,a[n];
+    printf("Enter your items : ");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+}
+void display(int n)
+{
+    int i,a[100];
+    printf("The elements of the array are :\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d",a[i]);
+    }
+}
+void main()
+{
+    int i;
+    int ch;
+    printf("Enter your choice:\n1.Create an array\n2.Display an array\n3.exit");
+    scanf("%d",&ch);
+    while(i)
+    {
+        switch(ch)
+        {
+            case 1 : printf("create an array: ");
+                     create(10);
+                     break;
+
+            case 2 : printf("Display an array: ");
+                     display(10);
+                     break;
+            case 3 : exit(0);
+
+            default: printf("Invalid option");
+                     break;
+
+        }
+    }
+
+
+}
