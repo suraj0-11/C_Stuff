c1180db900e73ae29acc11798b60959ae2670ce2
Suraj S <116282102+suraj0-11@users.noreply.github.com>
2023-01-19 11:38:51 +0530

    Update and rename c-formatter-beautifier (3).txt to Structex.c

--- Structex.c
+++ Structex.c
@@ -8,13 +8,10 @@ struct person {
 int main() {
   struct person * personPtr, person1;
   personPtr = & person1;
-
   printf("Enter age: ");
   scanf("%d", & personPtr -> age);
-
   printf("Enter weight: ");
   scanf("%f", & personPtr -> weight);
-
   printf("Displaying:\n");
   printf("Age: %d\n", personPtr -> age);
   printf("weight: %f", personPtr -> weight);
